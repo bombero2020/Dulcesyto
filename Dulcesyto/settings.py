aecaf2b0c7b6267290d09953dec7eb6035790668
marcelo <marceloarizagaforonda@gmail.com>
2020-11-25 13:03:33 +0100

    changed the style on fonts.

--- Dulcesyto/settings.py
+++ Dulcesyto/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = uuid.uuid4()
+with open('secret') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
